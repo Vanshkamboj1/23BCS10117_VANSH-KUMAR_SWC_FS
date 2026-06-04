d0aed3d3dde0e21d99fc167ad3c753afd6214864
Vansh Kumar <jaiyavanshkamboj@gmail.com>
2026-06-04 10:37:10 +0530

    Use effect

--- REACT/Button/src/App.jsx
+++ REACT/Button/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const[x, setX] = useState(1);
+  const[x, setX] = useState(0);
   const [count, setCount] = useState(1);
   const handle = () =>{
     setX(x+1);
-    if(x ===3){
-      setCount(count*2);
-      setX(1);
-    }
   }
+  useEffect(()=>{
+      if(x ===3){
+      setCount(count*2);
+      setX(0);
+    }
+  },[x]);
   return (
     <div>
       <h1>Count: {count}</h1>
